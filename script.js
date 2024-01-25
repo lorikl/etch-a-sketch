d8c8113e5adc36f001c9f226b31633bfe6271adc
Lorik Latifi <Lorikkatifi@gmail.com>
2024-01-24 21:54:54 -0500

    rough draft

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-// cube = document.getElementsByClassName("gamecube")
+let cube = document.querySelector(".gamecube")
+let size = document.querySelector('.input').value
+let clear = document.querySelector('.clear')
+let square = document.querySelectorAll(".gamecube > .square")
 
 function makeGrid (size) {
-    let cube = document.querySelector(".gamecube")
     cube.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     cube.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (i = 0; i < size * size; i++){
         let square = document.createElement('div')
+        square.className = "square"
         cube.insertAdjacentElement('beforeend', square)
+        square.addEventListener('click', colorGrid )
+        function colorGrid() {
+            square.style.backgroundColor = 'black'
+        }
     }
 }
 
 makeGrid(16)
+
+function eraser() {
+    cube.style.backgroundColor = "white"
+    console.log('reset')
+}
+
+clear.addEventListener('click', eraser)
+
