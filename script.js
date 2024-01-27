6e86454c77422f67c6caed12ca615f0532be1c2d
Lorik Latifi <Lorikkatifi@gmail.com>
2024-01-27 16:48:14 -0500

    event target scope fixed

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 let cube = document.querySelector(".gamecube")
-let size = document.querySelector('.input').value
-let clear = document.querySelector('.clear')
-let square = document.querySelectorAll(".gamecube > .square")
+let size = document.querySelector(".input")
+let clear = document.querySelector(".clear")
 
-function makeGrid (size) {
-    cube.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    cube.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    for (i = 0; i < size * size; i++){
-        let square = document.createElement('div')
-        square.className = "square"
-        cube.insertAdjacentElement('beforeend', square)
-        square.addEventListener('click', colorGrid )
-        function colorGrid() {
-            square.style.backgroundColor = 'black'
-        }
-    }
+function makeGrid(size) {
+	cube.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+	cube.style.gridTemplateRows = `repeat(${size}, 1fr)`
+	for (i = 0; i < size * size; i++) {
+		let square = document.createElement("div")
+		square.className = "square"
+		cube.insertAdjacentElement("beforeend", square)
+		square.addEventListener("click", colorGrid)
+		clear.addEventListener("click", eraser)
+	}
 }
 
-makeGrid(16)
-
-function eraser() {
-    cube.style.backgroundColor = "white"
-    console.log('reset')
+function colorGrid(e) {
+    e.target.style.backgroundColor = "black"
+}
+function eraser(e) {
+    e.target.style.backgroundColor = "white"
 }
 
-clear.addEventListener('click', eraser)
-
+size.value ? makeGrid(size.value) : makeGrid(16)
