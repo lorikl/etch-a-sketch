a992884e0b0a95a0a1b520632ef41590edce61c0
Lorik Latifi <Lorikkatifi@gmail.com>
2024-01-30 22:05:13 -0500

    updates to js and html

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let cube = document.querySelector(".gamecube")
 let size = document.querySelector(".input")
 let clear = document.querySelector(".clear")
+let draw = document.querySelector('.draw')
+let eraser = document.querySelector('.eraser')
 
 function makeGrid(size) {
 	cube.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -9,16 +11,33 @@ function makeGrid(size) {
 		let square = document.createElement("div")
 		square.className = "square"
 		cube.insertAdjacentElement("beforeend", square)
-		square.addEventListener("click", colorGrid)
+		square.addEventListener("mouseover", colorGrid)
 		clear.addEventListener("click", eraser)
+        function eraser() {
+            square.style.backgroundColor = "white"
+        }
 	}
 }
 
-function colorGrid(e) {
-    e.target.style.backgroundColor = "black"
-}
-function eraser(e) {
-    e.target.style.backgroundColor = "white"
+let color = true
+
+function setBlack() {
+    color = true
 }
 
+function setWhite() {
+    color = false
+}
+
+draw.onclick = () => setBlack
+eraser.onclick = () => setWhite
+
+function colorGrid(e, color) {
+    if (color = true){
+        e.target.style.backgroundColor = 'black'
+    } else if ( color = 2){
+        e.target.style.backgroundColor = 'white'
+    } 
+}
+console.log(color)
 size.value ? makeGrid(size.value) : makeGrid(16)
